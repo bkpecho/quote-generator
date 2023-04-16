d41ca26d26a42df4416498358132192ac26e3505
woprandis09 <bkpecho05@gmail.com>
2023-04-16 10:36:13 +0800

    add loading spinner

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
+function showLoadingSpinner() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+function removeLoadingSpinner() {
+    if(!loader.hidden){
+        quoteContainer.hidden = false
+        loader.hidden = true
+    }
+}
 // Get Quote From API
 
 async function getQuote() {
-    
+    showLoadingSpinner()
+
     /*
     NOTE: proxyURL is a fix to this error:
 
@@ -40,11 +53,11 @@ async function getQuote() {
         }
         quoteText.innerText = data.quoteText
 
+        removeLoadingSpinner()
 
     } catch (error) {
         //recall getQuote() if encounters error
-        // getQuote()
-        console.log('No Quote', error)
+        getQuote()
     }
 }
 
